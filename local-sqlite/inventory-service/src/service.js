505a956beab6f023e2d9bb0d17ab5b58d4c5f658
dglass-cellaware <daniel.glass@cellawaretech.com>
2020-06-15 13:03:56 -0500

    Initial commit

---
+++ local-sqlite/inventory-service/src/service.js
@@ -0,0 +1,12 @@
+const { ApolloServer } = require('apollo-server')
+const typeDefs = require('./graphql/schema')
+const resolvers = require('./graphql/resolvers')
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server
+    .listen()
+    .then(({ url }) => console.log('Server is running on localhost:4000'))
