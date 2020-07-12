5ee24fafe9b48ba46cf0345f3549954a93de421f
Michael Fabbri <michaelgfabbri@gmail.com>
2020-07-12 15:53:28 -0400

    added MOCHA, started test cases

--- sandbox/rdbms-sqlite/inventory-service/test/inventory.test.js
+++ sandbox/rdbms-sqlite/inventory-service/test/inventory.test.js
@@ -4,24 +4,27 @@ const { query, mutate } = createTestClient(server);
 
 /* Helper functions */
 
-module.exports = {
-
-    getTestPrtnum1() {
-        return "TEST001";
-    },
-
-    createTestInventory(inv_id) {
-        var res = await query({
-            query: "createInventory",
-            variables: {
-                input: {
-                    inv_id: inv_id,
-                    inv_uom: "TEST",
-                    inv_par_id: null,
-                    inv_ctr_id: null,
-                    attributes: null,
-                },
-            }
-        });
-    }
+function createTestInventory(inv_id) {
+    var res = await query({
+        query: "createInventory",
+        variables: {
+            input: {
+                inv_id: inv_id,
+                inv_uom: "TEST",
+                inv_par_id: null,
+                inv_ctr_id: null,
+                attributes: null,
+            },
+        }
+    });
 }
+
+/* Test Cases */
+
+describe('Create Inventory', function() {
+    describe('#create inventory api call', function() {
+        it('should create inventory with id "Test0001"', function() {
+            createTestInventory("Test0001");
+        })
+    }) 
+})
--- sandbox/rdbms-sqlite/inventory-service/test/move-inventory.test.js
+++ sandbox/rdbms-sqlite/inventory-service/test/move-inventory.test.js
@@ -1,1 +1,5 @@
 var assert = require('assert')
+
+const { createTestClient } = require('apollo-server-testing');
+const { query, mutate } = createTestClient(server);
+
